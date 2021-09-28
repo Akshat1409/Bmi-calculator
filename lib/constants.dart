60982c05f8f2f72de710013337b2f198483b442f
Akshat1409 <63799070+Akshat1409@users.noreply.github.com>
2021-09-29 00:47:27 +0530

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const double kBottomheight = 60.0;
+const kActivecolor = Color(0xFF1D1E33);
+const kInactivecolor = Color(0xFF0A0E21);
+const kBottomcolor = Color(0xFFEB1555);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 45.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kresultTextStyle = TextStyle(
+  fontSize: 22.0,
+  color: Colors.green,
+  fontWeight: FontWeight.bold,
+);
+const kBmiTextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
